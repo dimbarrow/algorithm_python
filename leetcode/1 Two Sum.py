92188695e74e09c7625b01346714a996d3722cab
Buzylev_Dima <Dimbarrow26@gmail.com>
2025-09-09 11:10:29 +0300

    all

--- leetcode/1 Two Sum.py
+++ leetcode/1 Two Sum.py
@@ -3,8 +3,6 @@ def two_sum(nums, target):
     for i in range(len(nums)):
         if target - nums[i] in d_dict:
             return [d_dict[target - nums[i]], i]
-        else:
-            d_dict[nums[i]] = i
-
+        d_dict[nums[i]] = i
 
 print(two_sum([2,7,11,15], 9))
