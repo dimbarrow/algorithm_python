6579dded56154d936ba991dafcd9aca4097a4ef5
Buzylev_Dima <Dimbarrow26@gmail.com>
2025-09-09 08:42:03 +0300

    all

--- leetcode/1 Two Sum.py
+++ leetcode/1 Two Sum.py
@@ -1,1 +1,11 @@
-rgfaurhfgnal/f
+def two_sum(nums, target):
+    dict = {}
+    dict[0] = nums[0]
+    for i in range(len(nums)):
+        if target - nums[i] not in dict:
+            dict[i] = nums[i]
+        else:
+            return [dict[nums[i]], i]
+
+
+print(two_sum([2,7,11,15], 9))
