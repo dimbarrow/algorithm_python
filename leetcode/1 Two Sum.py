dd17d8e89ff78af65fd889137a6b10906af55c5c
Buzylev_Dima <Dimbarrow26@gmail.com>
2025-09-09 08:45:40 +0300

    all

--- leetcode/1 Two Sum.py
+++ leetcode/1 Two Sum.py
@@ -1,11 +1,11 @@
 def two_sum(nums, target):
     dict = {}
-    dict[0] = nums[0]
+    dict[nums[0]] = 0
     for i in range(len(nums)):
         if target - nums[i] not in dict:
-            dict[i] = nums[i]
+            dict[nums[i]] = i
         else:
-            return [dict[nums[i]], i]
+            return [dict[target - nums[i]], i]
 
 
 print(two_sum([2,7,11,15], 9))
