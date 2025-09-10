c44aec851c187b4d060337ee522a409540f6c9da
Buzylev_Dima <Dimbarrow26@gmail.com>
2025-09-10 08:09:38 +0300

    all

--- leetcode/9 isPalindrome.py
+++ leetcode/9 isPalindrome.py
@@ -1,9 +1,7 @@
 def isPalindrome(x):
     x_l = str(x)
 
-    print(x_l[:len(x_l) // 2])
-    print(x_l[::-len(x_l) // 2])
-    if x_l[:len(x_l)//2] == x_l[::-len(x_l)//2]:
+    if x_l[:len(x_l)] == x_l[::-1]:
         return True
 
     return False
