cf33dac6d68f17000d5b55da56593b0be15d1256
Buzylev_Dima <Dimbarrow26@gmail.com>
2025-09-17 08:50:00 +0300

    all

--- leetcode/14 longest_common_prefix.py
+++ leetcode/14 longest_common_prefix.py
@@ -1,25 +1,44 @@
 def longestCommonPrefix(strs):
-        st = set()
-
-        for i in range(len(strs)):
-            st.add(strs[i][0])
-        if len(st) != 1:
-            return False
-
-        st = set()
-        minim = strs[0]
-        for i in range(len(strs)):
-            if strs[0] > strs[i]:
-                minim = strs[i]
-
-        counter = 0
-        for i in range(len(strs)):
-            for g in range(len(minim)):
-                st.add(strs[g][i])
-            if len(st) == i:
-                counter += 1
-            else:
-                return strs[0][counter]
+    st = set()
 
 
-print(longestCommonPrefix(["flower","flow","flight"]))
+    if strs == [""]:
+        return ""
+
+    for i in range(len(strs)):
+        if strs[i] == "":
+            return ""
+
+    for i in range(len(strs)):
+        st.add(strs[i][0])
+
+    if len(st) != 1:
+        return ""
+
+    st = set()
+    minim = strs[0]
+    for i in range(len(strs)-1):
+        if len(strs[i]) > len(strs[i+1]):
+            minim = strs[i+1]
+
+    maxim = 0
+    for i in range(len(strs)-1):
+        if len(strs[i]) < len(strs[i + 1]):
+            maxim = i+1
+
+    counter = 0
+    for i in range(len(minim)):
+        for g in range(len(strs)):
+            st.add(strs[g][i])
+            print(st)
+        if len(st) == i + 1:
+            counter += 1
+        elif len(st) != i + 1:
+            return strs[maxim][:counter]
+        else:
+            return strs[0]
+
+    return strs[maxim][:counter]
+
+
+print(longestCommonPrefix(["a","a","a"]))
