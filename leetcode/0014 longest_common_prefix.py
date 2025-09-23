cf024ab0068047a6652867eb33de41e1992e6297
Buzylev_Dima <Dimbarrow26@gmail.com>
2025-09-23 08:14:20 +0300

    all

--- leetcode/0014 longest_common_prefix.py
+++ leetcode/0014 longest_common_prefix.py
@@ -1,19 +1,15 @@
 def longestCommonPrefix(strs):
     minimum = strs[0]
     ind_min = 0
-    for i in range(len(strs)-1):
-        if len(strs[i]) < len(strs[i + 1]):
+    for i in range(1, len(strs)):
+        if minimum > strs[i]:
             minimum = strs[i]
-            ind_min = i
-        else:
-            minimum = strs[i+1]
-            ind_min = i
     print(minimum)
 
     new_strs = ""
     for i in range(len(minimum)):
         for k in range(len(strs)):
-            if strs[k][i] != strs[ind_min][i]:
+            if strs[k][i] != strs[i][i]:
                 return new_strs
         new_strs += strs[ind_min][i]
 
