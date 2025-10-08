0281f5702cf3af6cc3e4f97b080bb5ec9053bb84
Buzylev_Dima <Dimbarrow26@gmail.com>
2025-10-08 08:47:49 +0300

    all

--- leetcode/0003 longest_substring_without_repeating_characters.py
+++ leetcode/0003 longest_substring_without_repeating_characters.py
@@ -3,16 +3,24 @@ def lengthOfLongestSubstring(s):
     first = 0
     second = 0
     maxi = 0
-    for i in range(len(s)):
+    if s == " ":
+        return 1
+    while first != len(s):
         if s[first] not in substring:
             substring.add(s[first])
             first += 1
+            print('байден')
+            print(substring)
         else:
             maxi = max(maxi, len(substring))
+            print('обама')
+            print(maxi)
             substring.remove(s[second])
             second += 1
+            print('трамп')
+            print(substring)
 
     return maxi
 
 
-print(lengthOfLongestSubstring("abcabcbb"))
+print(lengthOfLongestSubstring("pwwkew"))
