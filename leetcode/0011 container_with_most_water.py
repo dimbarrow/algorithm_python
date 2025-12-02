173cdef69eafcee209bbc1c2cb10d0895ac9b822
Buzylev_Dima <Dimbarrow26@gmail.com>
2025-12-02 11:50:09 +0300

    all

---
+++ leetcode/0011 container_with_most_water.py
@@ -0,0 +1,17 @@
+def maxArea(height):
+    left = 0
+    right = len(height)-1
+    V_max = []
+    while left != right:
+        V_max.append((height.index(height[right]) - height.index(height[left])) * min(height[left], height[right]))
+        print(height.index(height[left]))
+        print(height.index(height[right]))
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return V_max
+
+
+print(maxArea([1,8,6,2,5,4,8,3,7]))
