42c155bc307ada3a651452f0eda6c43dd7927a5e
Buzylev_Dima <Dimbarrow26@gmail.com>
2026-01-13 08:15:37 +0300

    all

--- leetcode/0242 valid_anagram.py
+++ leetcode/0242 valid_anagram.py
@@ -1,16 +1,5 @@
+from collections import Counter
 def isAnagram(s, t):
-    s_set = set()
-    t_set = set()
-    for i in range(len(s)):
-        s_set.add(s[i])
-
-    for i in range(len(t)):
-        t_set.add(t[i])
-
-    if s_set == t_set:
-        return True
-
-    return False
-
+    return Counter(s) == Counter(t)
 
 print(isAnagram('rat', 'car'))
